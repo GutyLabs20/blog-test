1321d13f2cecb444a9f1331a7b7e474d08ceb866
GutyLabs <gutylabs20@gmail.com>
2021-01-12 01:57:04 -0500

    Creacion de Seeder para User y Factories para User, Tag, Post, Image y con eso se lleno datos falsos a la base de datos. Revisar ImageFactory

---
+++ database/factories/ImageFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Image;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ImageFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Image::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+            'url' => 'posts/' . $this->faker->image('public/storage/posts', 640, 480, null, false)
+        ];
+    }
+
+    //Para cargar las imagenes en la carpeta /storage/app/public
+    // se tiene que ir al archivo /config/filesystems.php y
+    // en 'default' => env('FILESYSTEM_DRIVER', 'local'),
+    // cambiarlo a public
+
+    // Creando seeder para categorias y user
+}
