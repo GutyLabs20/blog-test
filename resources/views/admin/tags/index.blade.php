bdf57f1631088d765f5c4e8b39d16e314f182167
GutyLabs <gutylabs20@gmail.com>
2021-03-02 17:59:31 -0500

    Se realizo el CRUD de Etiquetas, agregando un select option

---
+++ resources/views/admin/tags/index.blade.php
@@ -0,0 +1,60 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+
+    <a class="btn btn-primary btn-sm float-right" href="{{route('admin.tags.create')}}">Agregar Nueva Etiqueta</a>
+
+    <h1>Listar Tags</h1>
+@stop
+
+@section('content')
+
+    @if (session('info'))
+        <div class="alert alert-success">
+            <strong>{{session('info')}}</strong>
+        </div>
+    @endif
+
+    <div class="card">
+        {{-- <div class="card-header">
+            <a class="btn btn-primary btn-sm" href="{{route('admin.tags.create')}}">Agregar Tag</a>
+        </div> --}}
+        <div class="card-body">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th colspan="2"></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach($tags as $tag)
+                        <tr>
+                            <td>{{$tag->id}}</td>
+                            <td>{{$tag->name}}</td>
+                            <td width="10px">
+                                <a class="btn btn-primary btn-sm" href="{{route('admin.tags.edit', $tag)}}">Editar</a>
+                            </td>
+                            <td width="10px">
+                                <form action="{{route('admin.tags.destroy', $tag)}}" method="POST">
+                                    @csrf
+                                    @method('delete')
+
+                                    <button type="submit" class="btn btn-danger btn-sm">
+                                        Eliminar
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+@stop
+
