49ddca3e7e3dc06fdf6e75570a468824385cc5e7
GutyLabs <gutylabs20@gmail.com>
2021-03-07 23:29:41 -0500

    Cambio de seeder User, asignandolo metodo assignRole

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,12 +14,12 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //Con los roles, se agregara el metodo assignRole con el parametro
         User::create([
             'name' => 'José Luis GutyLabs',
             'email' => 'admin@blog.test',
             'password' => bcrypt('password')
-        ]);
+        ])->assignRole('Admin');
 
         User::factory(9)->create();
     }
